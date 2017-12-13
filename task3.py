aacf9d2588be8ea4297f87bcc3df00e12deb1d25
Герасимов Дмитрий Александрович <Gerasimov_DA@mgtniip.ru>
2017-12-13 15:22:11 +0300

    update task3.py

--- task3.py
+++ task3.py
@@ -3,35 +3,49 @@ import sys
 
 
 class ArgumentParser:
-    def __init__(self, argumet_struct):
+    """Class to handle arguments parse.
+
+    Initialize with a argument dictionary, example below.
+    Use parse method to parse arg list."""
+
+    def __init__(self, argumet_struct: dict):
+        """Init parser with argument_struct."""
         self._parser = argparse.ArgumentParser(description='My task3 parser')
-        for arg_name, arg_rules in arg_struct.items():
+        for arg_name, arg_rules in argumet_struct.items():
             help_message = 'Set {} param{}'.format(
                 arg_name, ', required' if arg_rules['required'] else '')
-            self._parser.add_argument(
-                arg_name, help=help_message, required=arg_rules['required'],
-                type=arg_rules['type'])
+            if arg_rules['type'] == bool:
+                self._parser.add_argument(
+                    arg_name, help=help_message,
+                    required=arg_rules['required'],
+                    default=False, action='store_true')
+            else:
+                self._parser.add_argument(
+                    arg_name, help=help_message,
+                    required=arg_rules['required'],
+                    default=None, type=arg_rules['type'])
 
-    def parse(self, arg_list):
+    def parse(self, arg_list: list):
+        """Parse an argument list, sys.argv[1:] for example."""
         return self._parser.parse_args(arg_list)
 
 
-arg_struct = {
-    '--int': {
-        'type': int,
-        'required': True},
-    '--str': {
-        'type': str,
-        'required': True},
-    '--bool': {
-        'type': lambda x: False if x in ['0', 'F', 'f', 'False'] else True,
-        'required': False},
-    '--float': {
-        'type': int,
-        'required': False}}
-
-
 if __name__ == '__main__':
+
+    arg_struct = {
+        '--int': {
+            'type': int,
+            'required': True},
+        '--str': {
+            'type': str,
+            'required': True},
+        '--bool': {
+            'type': bool,
+            'required': False},
+        '--float': {
+            'type': float,
+            'required': False}}
+
     parser = ArgumentParser(arg_struct)
     if len(sys.argv) > 1:
         args = parser.parse(sys.argv[1:])
